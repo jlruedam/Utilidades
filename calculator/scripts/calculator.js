f31d3bb555fa0425640cf397505b38f4754ffaf0
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-01-19 14:08:54 -0500

    Agregar mas botones y algunas funcionalidades para mostrar en el screen de la calculadora

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,7 +1,48 @@
 var keyboard=document.getElementById("keyboard");
-keyboard.addEventListener("click", prueba, false);
+var screenCalculator=document.getElementById("screen");
+keyboard.addEventListener("click", calculate, false);
 
-function prueba(e){
+var showScreen="";
+
+function calculate(e){
+    var key=e.path[0].value;
+    const operators="+-/*.=";
+    const digits="0123456789";
+    const actions=["CE", "C","root"]
+
+    if(actions.includes(key)){
+
+        if(key=="CE"){
+
+            showScreen=showScreen.slice(0,-1);
+            screenCalculator.value=showScreen;
+
+        }
+
+        if(key=="C"){
+
+            screenCalculator.value="";
+
+        }
+    }
+
+    if (digits.includes(key)){
+
+        showScreen+=key;
+        screenCalculator.value=showScreen;
+
+    }
+    
+    if(operators.includes(key)){
+
+       if(!operators.includes(showScreen[showScreen.length-1])){
+
+           showScreen+=key;
+           screenCalculator.value=showScreen;
+
+       }
+    }
+        
     console.log(e.path[0].value);
 }
 
